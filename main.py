043595ca8421f49f1d54c5efcd54375e1d47395b
Charanvir <charanvir123@gmail.com>
2023-02-20 02:16:58 -0500

    integrated chatbot into chatbot window, added error handling for empty user input and model techincal difficultues from openai server

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import sys
 
-from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton, QApplication
+import openai.error
+
+from backend import Chatbot
+
+from PyQt6.QtWidgets import QMainWindow, QTextEdit, QLineEdit, QPushButton, QApplication, QMessageBox
 
 
 class ChatbotWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+
+        # Best to be instantiated once when the ChatbotWindow is first instantiated
+        self.chat = Chatbot()
 
         self.setMinimumSize(700, 500)
 
@@ -29,8 +36,29 @@ class ChatbotWindow(QMainWindow):
 
         self.button = QPushButton("Send", self)
         self.button.setGeometry(500, 340, 100, 40)
+        self.button.clicked.connect(self.send_message)
 
         self.show()
+
+    def send_message(self):
+        user_input = self.inputField.text().strip()
+        if user_input == "":
+            error_widget = QMessageBox()
+            error_widget.setWindowTitle("Error")
+            error_widget.setText("Input was empty.")
+            error_widget.exec()
+            return
+        self.chatArea.append(f"Me: {user_input}")
+        self.inputField.clear()
+        try:
+            response = self.chat.get_response(user_input)
+            self.chatArea.append(f"Bot: {response}")
+        except openai.error.RateLimitError or openai.error.APIError:
+            error_widget = QMessageBox()
+            error_widget.setWindowTitle("Error")
+            error_widget.setText("The model is currently experiencing some difficulties, please try again later.")
+            error_widget.exec()
+            return
 
 
 app = QApplication(sys.argv)
